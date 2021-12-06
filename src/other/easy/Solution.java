dff744474653ba6ad85bc0151b454b9624ac04de
lupeiyao <lupeiyao@corp.netease.com>
2021-12-06 20:59:54 +0800

    refactor dir

--- src/other/easy/Solution.java
+++ src/other/easy/Solution.java
@@ -1,4 +1,4 @@
-package easy;
+package other.easy;
 
 /**
  * @Author Lunus
--- src/tree/middle/BST.java
+++ src/tree/middle/BST.java
@@ -1,14 +1,15 @@
-package middle.tree;
+package tree.middle;
 
 import java.util.LinkedList;
 
 /**
  * @Author Lunus
- * @Description TODO
+ * @Description 剑指 Offer II 055. 二叉搜索树迭代器 https://leetcode-cn.com/problems/kTOapQ/
  * @Date 2021/11/7 20:51
  * @Version 1.0
  */
-public class Solution {
+
+public class BST {
 
     public static void main(String[] args) {
         TreeNode root = new TreeNode(7);
@@ -35,10 +36,7 @@ public class Solution {
 
 }
 
-/*
-剑指 Offer II 055. 二叉搜索树迭代器
-https://leetcode-cn.com/problems/kTOapQ/
- */
+
 class BSTIterator {
 
     LinkedList<TreeNode> travelNodes = new LinkedList<>();
